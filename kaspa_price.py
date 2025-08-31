1b46955f1aa42965b981dcb95ff368d6a391a956
Giorgos Tsaloukidis <itsalou23@gmail.com>
2025-08-31 18:38:30 +0300

    update kaspa_price logic

--- kaspa_price.py
+++ kaspa_price.py
@@ -1,6 +1,10 @@
 # kaspa_price.py
 
 import requests
+import csv
+from datetime import datetime
+import os
+print("Saving CSV in:", os.getcwd())
 
 def get_kaspa_price():
     url = "https://api.coingecko.com/api/v3/simple/price"
@@ -18,10 +22,22 @@ def get_kaspa_price():
     except requests.exceptions.RequestException as e:
         print("Error fetching price:", e)
         return None
+def save_price_to_csv(price,filename="kaspa_price.csv"):
+    timestamp=datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    file_exists=os.path.isfile(filename)
+
+    with open(filename, mode="a",newline="") as file:
+        writer =csv.writer(file)
+        if not file_exists:
+            writer.writerow(["timestamp","price_usd"])
+        writer.writerow([timestamp,price])
+
 
 if __name__ == "__main__":
     price = get_kaspa_price()
     if price is not None:
         print(f"Current Kaspa price: ${price}")
+        save_price_to_csv(price)
+        print("Price saved to CSV.")
     else:
         print("Could not fetch Kaspa price.")
